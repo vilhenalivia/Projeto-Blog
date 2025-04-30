ebc6a98e726ec7f197f6aaa865d6d1fce9d888d8
Livia Vilhena <liviavilhena@outlook.com>
2025-04-30 10:56:20 -0300

    Configuração do Dockerfile para gerar a imagem Docker Django

---
+++ scripts/commands.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# TUDO SERÁ EXECUTADO QUANDO DER DOKERCOMPOSEUP
+
+# O shell irá encerrar a execução do script quando um comando falhar
+set -e
+
+while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+  echo "🟡 Waiting for Postgres Database Startup ($POSTGRES_HOST $POSTGRES_PORT) ..."
+  sleep 0.1
+done
+
+echo "✅ Postgres Database Started Successfully ($POSTGRES_HOST:$POSTGRES_PORT)"
+
+python manage.py collectstatic
+python manage.py migrate
+python manage.py runserver
