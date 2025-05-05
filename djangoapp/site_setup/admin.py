b5bca377935823e143a4e46cc10f109ac4cb6d30
Livia Vilhena <liviavilhena@outlook.com>
2025-05-05 15:52:07 -0300

    Criação de um novo model (SiteStups) para a admin e ajuste de permissões da mesma

--- djangoapp/site_setup/admin.py
+++ djangoapp/site_setup/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from site_setup.models import MenuLink
+from site_setup.models import MenuLink, SiteSetup
 
 # Register your models here.
 @admin.register(MenuLink)
@@ -7,3 +7,11 @@ class MenuLinkAdmin(admin.ModelAdmin):
     list_display = 'id', 'text', 'url_or_path',
     list_display_links = 'id', 'text', 'url_or_path',
     search_fields = 'id', 'text', 'url_or_path',
+
+@admin.register(SiteSetup)
+class SiteSetupAdmin(admin.ModelAdmin):
+    list_display = 'title', 'description',
+
+    # Permissão de adicionar novos valores na area administrativa -> No caso, não tera permissão se já tiver algum valor
+    def has_add_permission(self, request):
+        return not SiteSetup.objects.all()[:1].exists()
