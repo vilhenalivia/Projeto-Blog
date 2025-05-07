93bb0dc1bfd1cadd822eb03be34e76c3679d4ac2
Livia Vilhena <livia.vilhena@proxion.local>
2025-05-07 09:51:43 -0300

    Criação da base de templates necessários para o layout

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -6,5 +6,9 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('post/', views.post, name='post'),
+    path('page/', views.page, name='page'),
 ]
+
+
 
--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 #view INDEX
 def index(request):
     return render(request,'blog/pages/index.html')
+
+def page(request):
+    return render(request,'blog/pages/page.html')
+
+def post(request):
+    return render(request,'blog/pages/post.html')
