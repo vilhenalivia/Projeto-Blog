4f2238048213bd0124e61ab49334a9507cf078d9
Livia Vilhena <livia.vilhena@proxion.local>
2025-05-07 10:32:15 -0300

    Layout do blog

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,11 +1,28 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
+
+posts = list(range(1000))
 
 #view INDEX
 def index(request):
-    return render(request,'blog/pages/index.html')
+    paginator = Paginator(posts,9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    context ={ 'page_obj': page_obj, } 
+
+    return render(request,'blog/pages/index.html', context)
 
 def page(request):
-    return render(request,'blog/pages/page.html')
+    paginator = Paginator(posts,9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    context ={ 'page_obj': page_obj, }
+
+    return render(request,'blog/pages/page.html', context)
 
 def post(request):
-    return render(request,'blog/pages/post.html')
+    paginator = Paginator(posts,9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    context ={ 'page_obj': page_obj, }
+    return render(request,'blog/pages/post.html', context)
