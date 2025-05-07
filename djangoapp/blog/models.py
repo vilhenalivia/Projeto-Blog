5b222d18ef23d21af7e5397a9a122555acf46c46
vilhena.livia <liviavilhena@outlook.com>
2025-05-07 12:56:05 -0300

    Criação dos models Category e Page e configurações delas na area administrativa

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from utils.rands import slugify_new
 
 # Create your models here.
+
+# MODEL TAGS
 class Tag(models.Model):
     class Meta:
         verbose_name = 'Tag'
@@ -16,6 +18,49 @@ class Tag(models.Model):
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify_new/(self.name, 4)
+            self.slug = slugify_new(self.name, 4)
         return super().save(*args, **kwargs)
     
+    def __str__(self):
+        return self.name
+
+
+# MODEL CATEGORY
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(
+        unique=True, default=None,
+        null=True, blank=True, max_length=255,
+    )
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify_new(self.name, 4)
+        return super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.name
+
+
+# MODEL PAGE
+class Page(models.Model):
+    title = models.CharField(max_length=65,)
+    slug = models.SlugField(
+        unique=True, default="",
+        null=True, blank=True, max_length=255,
+    )
+    is_published = models.BooleanField(default=False)
+    content = models.TextField()
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify_new(self.title, 4)
+        return super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.name
+
