895690a7a30bdf5d1ae65937921713d9306ed99e
vilhena.livia <liviavilhena@outlook.com>
2025-05-08 11:25:14 -0300

    Filtrando e ordenando os posts enviados para a view

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -6,7 +6,7 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('post/', views.post, name='post'),
+    path('post/<slug:slug>/', views.post, name='post'),
     path('page/', views.page, name='page'),
 ]
 
--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,11 +1,14 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render
+from blog.models import Post
 
-posts = list(range(1000))
+PER_PAGE = 9
 
 #view INDEX
 def index(request):
-    paginator = Paginator(posts,9)
+    posts = Post.objects.filter(is_published=True).order_by('-id')
+
+    paginator = Paginator(posts, PER_PAGE)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
     context ={ 'page_obj': page_obj, } 
@@ -13,16 +16,7 @@ def index(request):
     return render(request,'blog/pages/index.html', context)
 
 def page(request):
-    paginator = Paginator(posts,9)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
-    context ={ 'page_obj': page_obj, }
-
-    return render(request,'blog/pages/page.html', context)
+    return render(request,'blog/pages/page.html',)
 
 def post(request):
-    paginator = Paginator(posts,9)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
-    context ={ 'page_obj': page_obj, }
-    return render(request,'blog/pages/post.html', context)
+    return render(request,'blog/pages/post.html',)
